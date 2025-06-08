f7d87a23975e10a4d5dc8d6eb7caa5bd3a7f99e8
Muhammed Sanjid <sanjid357@gmail.com>
2025-06-08 20:12:32 +0530

    projects section

---
+++ components/home/projects-section.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { projects } from "@/constants/projects";
+import SectionHeader from "@/components/section-header";
+import { ArrowUpRight } from "lucide-react";
+import MotionDiv from "../motion-div";
+import { Link } from "next-view-transitions";
+
+const ProjectsSection = () => {
+  const project = projects.slice(0, 3);
+  return (
+    <div>
+      <SectionHeader
+        title="Projects"
+        description="A collection of projects I've worked on."
+      />
+      <div className="border-t border-border" />
+      <div className="space-y-8">
+        {project.map((project, index) => (
+          <MotionDiv
+            initial={{ opacity: 0, y: 20, filter: "blur(5px)" }}
+            animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+            transition={{ duration: 0.3, delay: index * 0.1 }}
+            key={project.title}
+            className="group space-y-4"
+          >
+            <Link target="_blank" rel="noopener noreferrer" href={project.link}>
+              <h2 className="mt-4 font-serif text-lg md:mt-8">
+                {project.title}
+              </h2>
+              <p className="text-muted-foreground">{project.description}</p>
+              <div className="flex flex-wrap gap-2">
+                {project.tech.map((tech) => (
+                  <span
+                    key={tech}
+                    className="rounded-md bg-muted px-2 py-1 text-xs text-muted-foreground"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="mt-2 flex items-center text-sm text-muted-foreground transition-colors hover:text-foreground">
+                <span className="underline decoration-dashed underline-offset-4">
+                  View project
+                </span>
+                <ArrowUpRight className="ml-1 h-3 w-3 transition-transform duration-200 ease-in-out group-hover:translate-x-2" />
+              </div>
+              {index < projects.length - 1 && (
+                <div className="mt-8 border-t border-border" />
+              )}
+            </Link>
+          </MotionDiv>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsSection;
